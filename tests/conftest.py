222319ebd7796c3742c60f2c1cd05ab2c7dcd9f5
ericsalesdeandrade <sdaeric19@gmail.com>
2024-10-10 07:55:41 +0100

    clean

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-# tests/conftest.py
 import pytest
 from dynaconf import settings
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,10 +2,8 @@ from src.app import get_database_url, is_debug_mode
 
 
 def test_database_url_testing_env():
-    """Test to verify that the DATABASE_URL from the 'testing' environment is used."""
     assert get_database_url() == "sqlite:///testing.db"
 
 
 def test_debug_mode_testing_env():
-    """Test to verify that DEBUG is True in the 'testing' environment."""
     assert is_debug_mode() is True
